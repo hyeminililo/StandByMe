b3f21cca58e5e75bb556af039788aa5aa4bb1ab4
NamGung Hyemin <ng040115@gmail.com>
2024-02-01 02:45:49 +0900

    add translaion

---
+++ lib/Model/common/translateButton.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter_sc/Model/common/client.dart';
+
+class Translation {
+  final dio = Dio();
+
+  Future<String> translatedText() async {
+    String translationResult = '';
+    dynamic response = await dio.post(
+        'https://openapi.naver.com/v1/papago/n2mt',
+        data: {
+          'source': 'ko',
+          'target': 'en',
+          'text': '이 부분에 받는것을 어떻게 해야할까 ,,'
+        },
+        options: Options(headers: {
+          'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+          'X-Naver-Client-Id': CLIENT_ID,
+          'X-Naver-Client-Secret': CLIENT_SECRET,
+        }));
+
+    if (response.statusCode == 200 || response.statusCode == 201) {
+      final data = response.data;
+      final translatedText = data['message']['result']['translatedText'];
+      translationResult = translatedText;
+    } else {
+      translationResult = '번역 실패 : ${response.statusCode}';
+    }
+
+    return translationResult;
+  }
+}
+
+/*
+class TranslateButton {
+  ElevatedButton elevatedButton;
+  TranslateButton({required this.elevatedButton}){
+    elevatedButton = ElevatedButton(onPressed: (){}, child: child)
+  }
+}*/
