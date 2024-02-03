605a96e0614625a00aef0a3d96cbe3154ed6d26c
NamGung Hyemin <ng040115@gmail.com>
2024-02-04 02:27:18 +0900

    create board ui

---
+++ lib/Screens/Board/board.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_sc/Model/common/widget/TabBar.dart';
+import 'package:flutter_sc/Model/common/widget/appBar.dart';
+
+class boardScreen extends StatefulWidget {
+  const boardScreen({super.key});
+
+  @override
+  State<boardScreen> createState() => _boardScreenState();
+}
+
+class _boardScreenState extends State<boardScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      appBar: AppBarBase(title: 'boardScreen'),
+      body: Column(
+        children: [
+          Row(
+            children: [Text('제목'), Text('$comment')],
+          ),
+          Row(
+            children: [Text('사진'), Text('$picture')],
+          ),
+        ],
+      ),
+      bottomNavigationBar: TabBarBase(),
+    );
+  }
+}
